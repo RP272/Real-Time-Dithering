0786c75a6b4d332f0ed6b83089f11a6eba173fa2
RP272 <272931@student.pwr.edu.pl>
2025-12-05 17:34:15 +0100

    feat: dithered cat gif

--- src/my-react-router-app/app/welcome/welcome.tsx
+++ src/my-react-router-app/app/welcome/welcome.tsx
@@ -3,7 +3,7 @@ import "./welcome.css";
 
 export function Welcome() {
   return (
-    <main className="flex flex-col items-center justify-center pt-16 pb-4 font-[Typewriter]">
+    <main className="flex flex-col items-center justify-center pt-16 pb-4 font-[Typewriter] h-dvh">
       <h1 className="text-3xl font-bold mb-4 text-center">Welcome to Real-Time Dithering</h1>
       <h2 className="text-xl mb-4 text-center">Try out below versions:</h2>
       <ul className="list-disc ditheringLinks">
@@ -14,6 +14,8 @@ export function Welcome() {
           <Link className="italic text-center" to="/BayerOrdered" >Bayer Ordered Dithering</Link>
         </li>
       </ul>
+
+      <img className="mt-10" src="https://64.media.tumblr.com/6e11dfdb4db8fdec29802e412072af28/tumblr_nhrls6xYjy1r36t2xo1_500.gif" alt="dithered cat"/>
     </main>
   );
 }
